1f1afbb2d5937a864e5fde0b7a4794cb0ca1b82d
moon <rycmoon@gmail.com>
2021-09-22 15:59:49 -0500

    Frontend: moved code around and now it displays login error message

--- frontend/src/Pages/Startup/Login.tsx
+++ frontend/src/Pages/Startup/Login.tsx
@@ -1,50 +1,45 @@
 import React, {useState} from 'react';
 import FormInput from "../../Components/Misc/FormInput";
 import {useHistory} from "react-router-dom";
-
-
-//region FETCH
-const submitLogin = async (data:{} = {})=>{
-    let url='/backend/submitLogin';
-    // Default options are marked with *
-    const response = await fetch(url, {
-        method: 'POST', // or 'PUT'
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success: ', data);
-        })
-        .catch((error) => {
-            console.error('Error: ', error);
-        });
-    return response;
-}
-
-
-//endregion
-
-
+import LogoutButton from "../../Components/Misc/LogoutButton";
 
 const LoginPage = () => {
     const history = useHistory();
-    const navigateToPage = ()=> history.push('/signup');
+    const navigateToSignup = ()=> history.push('/signup');
+
+    const [loginMessage, setLoginMessage] = useState("");
+
+    //region FETCH
+    const submitLogin = async (data:{} = {})=>{
+        let url='/users/submitLogin';
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(data => {
+                if(data.error){
+                    setLoginMessage(data.message);
+                }
+            })
+            .catch((error) => {
+                console.error('Error: ', error);
+            });
+        return response;
+    }
+    //endregion
 
     const [user, setUser] = useState({email: "", password: ""})
 
     const handleEmailCallBack = (e:React.ChangeEvent<HTMLInputElement>) => {
-
         setUser(user => ({...user, email: e.target.value}))
-        console.log("Email: " + e.target.value);
-
     }
     const handlePasswordCallBack = (e:React.ChangeEvent<HTMLInputElement>) => {
-
         setUser(user => ({...user, password: e.target.value}))
-        console.log("password: " + e.target.value);
     }
 
     return (
@@ -55,7 +50,10 @@ const LoginPage = () => {
             <h3>Password</h3>
             <FormInput type={"password"} value={user.password} onChange={handlePasswordCallBack}/><br/><br/>
             <button value={"Login"} onClick={()=>submitLogin(user)}>Login</button><br/><br/>
-            <button onClick={()=>navigateToPage()}>Create an account</button>
+            {loginMessage === "" ? <></>:<>{loginMessage}</>}
+            <br/><br/>
+            <button onClick={()=>navigateToSignup()}>Create an account</button>
+            <LogoutButton/>
         </div>
     );
 };
