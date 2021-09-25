341cc95b0a48d767f4b7b04050f660565e64c1aa
moon <rycmoon@gmail.com>
2021-09-25 17:21:21 -0500

    Added a component to get the value of the time

---
+++ frontend/src/Components/Misc/TimePickerComponent.tsx
@@ -0,0 +1,40 @@
+import React, {useEffect} from 'react';
+import MobileTimePicker from "@mui/lab/MobileTimePicker";
+import {TextField} from "@mui/material";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+
+interface ITimePickerComponentProps {
+    initialTime: Date,
+    label: String,
+    handleTimeOnChange: (e: any) => void
+}
+
+
+const TimePickerComponent = (props: ITimePickerComponentProps) => {
+
+    const [value, setValue] = React.useState<Date>(props.initialTime);
+
+    useEffect(() => {
+        props.handleTimeOnChange(value)
+    }, [value])
+
+    return (
+        <div>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                {/*<Stack spacing={3}>*/}
+                <MobileTimePicker
+                    label={props.label}
+                    value={value}
+                    onChange={(newValue) => {
+                        newValue == null ? setValue(new Date()) : setValue(newValue)
+                    }}
+                    renderInput={(params) => <TextField size={"small"} label={"Time of Dosage"} {...params} />}
+                />
+                {/*</Stack>*/}
+            </LocalizationProvider>
+        </div>
+    );
+};
+
+export default TimePickerComponent;
