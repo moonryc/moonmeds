3902654bde7bc0108a7ba67f528f00f49eb6e5cc
moon <rycmoon@gmail.com>
2021-10-02 18:53:22 -0500

    added in selectDay and setSelectedDay, both need to be moved to there own user context, this was just temporary

--- frontend/src/Components/Misc/UserContext.tsx
+++ frontend/src/Components/Misc/UserContext.tsx
@@ -5,6 +5,8 @@ interface IUserContextState{
     setLoggedIn:(state:boolean) => void,
     userId:string,
     setUserId:(state:string) => void,
+    selectedDay:Date,
+    setSelectedDay:(state:Date)=>void
 
 }
 
@@ -12,17 +14,19 @@ export const UserContext = createContext<IUserContextState>({
     loggedIn:false,
     setLoggedIn:(state:boolean)=>{},
     userId:'test',
-    setUserId:(state:string) => ''
+    setUserId:(state:string) => '',
+    selectedDay:new Date(),
+    setSelectedDay:(state:Date)=>{},
 })
 
 export const UserContainer= (props:any) => {
     const {children} = props;
     const [loggedIn, setLoggedIn] = useState<boolean>(false);
     const [userId, setUserId] = useState<string>('test');
-
+    const [selectedDay, setSelectedDay] = useState<Date>(new Date());
 
     return (
-        <UserContext.Provider value = {{loggedIn, setLoggedIn, userId, setUserId}}>
+        <UserContext.Provider value = {{loggedIn, setLoggedIn, userId, setUserId, selectedDay,setSelectedDay}}>
             {children}
         </UserContext.Provider>
     )
