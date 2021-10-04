7a72b603d88c93a3ee73fe6ba60a614a7641d5cb
moon <rycmoon@gmail.com>
2021-10-03 22:13:43 -0500

    date is rendered based on date rather than by index

--- frontend/src/Components/CalendarPageComponets/Calendar/CalendarDay.tsx
+++ frontend/src/Components/CalendarPageComponets/Calendar/CalendarDay.tsx
@@ -1,24 +1,25 @@
 import React, {useContext} from 'react';
 import {IconButton} from "@mui/material";
 import {ICalendarDay} from "../../../Types/CalendarType";
-import {UserContext} from "../../Misc/UserContext";
+import {CalendarContext} from "../CalendarContext";
+import {getDate} from "date-fns";
 
 
 
 
 const CalendarDay = (props:ICalendarDay) => {
 
-    const {setSelectedDay} = useContext(UserContext);
+    const {setSelectedDay} = useContext(CalendarContext);
 
     const handleOnDayClick = () => {
       setSelectedDay(props.date)
     }
 
-
+    //TODO: make it not render last months dates
     return (
         <div>
             <IconButton onClick={()=>handleOnDayClick()}>
-                 {props.index}
+                 {getDate(props.date)}
             </IconButton>
         </div>
     );
