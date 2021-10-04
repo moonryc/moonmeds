02a86166ed305fe2664cc5102c814834b54f9634
moon <rycmoon@gmail.com>
2021-10-03 22:12:36 -0500

    misc.

--- frontend/src/Components/CalendarPageComponets/DateDetails/DateDetailsHeader.tsx
+++ frontend/src/Components/CalendarPageComponets/DateDetails/DateDetailsHeader.tsx
@@ -6,8 +6,6 @@ const DateDetailsHeader = (props:ICalendarDay) => {
 
     const date = useMemo(() => toDate(props.date).toDateString(), [props.date])
 
-
-
     return (
         <div>
             <h6>Header</h6>
