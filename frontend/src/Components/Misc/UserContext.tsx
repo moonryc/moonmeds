084df2525124a8192d26b339387e4ec32b03bb30
moon <rycmoon@gmail.com>
2021-10-03 22:10:44 -0500

    removed unneeded props (these were moved to CalenderContext)

--- frontend/src/Components/Misc/UserContext.tsx
+++ frontend/src/Components/Misc/UserContext.tsx
@@ -5,9 +5,6 @@ interface IUserContextState{
     setLoggedIn:(state:boolean) => void,
     userId:string,
     setUserId:(state:string) => void,
-    selectedDay:Date,
-    setSelectedDay:(state:Date)=>void
-
 }
 
 export const UserContext = createContext<IUserContextState>({
@@ -15,18 +12,17 @@ export const UserContext = createContext<IUserContextState>({
     setLoggedIn:(state:boolean)=>{},
     userId:'test',
     setUserId:(state:string) => '',
-    selectedDay:new Date(),
-    setSelectedDay:(state:Date)=>{},
+
 })
 
 export const UserContainer= (props:any) => {
     const {children} = props;
     const [loggedIn, setLoggedIn] = useState<boolean>(false);
     const [userId, setUserId] = useState<string>('test');
-    const [selectedDay, setSelectedDay] = useState<Date>(new Date());
+
 
     return (
-        <UserContext.Provider value = {{loggedIn, setLoggedIn, userId, setUserId, selectedDay,setSelectedDay}}>
+        <UserContext.Provider value = {{loggedIn, setLoggedIn, userId, setUserId}}>
             {children}
         </UserContext.Provider>
     )
