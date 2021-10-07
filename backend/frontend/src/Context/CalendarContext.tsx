9dd46b738af185a2ca67b9174a366543a03c78f8
moon <rycmoon@gmail.com>
2021-10-06 23:31:35 -0500

    added 2 new properties

--- backend/frontend/src/Context/CalendarContext.tsx
+++ backend/frontend/src/Context/CalendarContext.tsx
@@ -1,21 +1,27 @@
 import React, {createContext, useState} from "react";
+import {IMedicationDosagesSchema} from "../../../Types/MedicationType";
 
 export interface ICalendarContextState {
     selectedDay:Date,
     setSelectedDay:(state:Date)=>void
+    selectedDayDetails:IMedicationDosagesSchema[]
+    setSelectedDayDetails:(state:IMedicationDosagesSchema[])=>void
 }
 
 export const CalendarContext = createContext<ICalendarContextState>({
     selectedDay:new Date(),
     setSelectedDay:(state:Date)=>{},
+    selectedDayDetails:[],
+    setSelectedDayDetails:(state:IMedicationDosagesSchema[])=>{}
 })
 
 export const CalendarContainer = (props:any)=>{
     const {children} = props;
     const [selectedDay, setSelectedDay] = useState<Date>(new Date());
+    const [selectedDayDetails, setSelectedDayDetails] = useState<IMedicationDosagesSchema[]>([]);
 
     return(
-        <CalendarContext.Provider value={{selectedDay,setSelectedDay}}>
+        <CalendarContext.Provider value={{selectedDay,setSelectedDay,selectedDayDetails,setSelectedDayDetails}}>
             {children}
         </CalendarContext.Provider>
     )
