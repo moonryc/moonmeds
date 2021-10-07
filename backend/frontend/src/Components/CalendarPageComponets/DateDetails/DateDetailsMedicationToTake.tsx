b2db96440baa3b0fce9c50ff9714ed88f31d0793
moon <rycmoon@gmail.com>
2021-10-06 23:32:35 -0500

    misc.

--- backend/frontend/src/Components/CalendarPageComponets/DateDetails/DateDetailsMedicationToTake.tsx
+++ backend/frontend/src/Components/CalendarPageComponets/DateDetails/DateDetailsMedicationToTake.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {ICalendarDay} from "../../../../../Types/CalendarType";
 import {Typography} from "@mui/material";
+import {CalendarContext} from "../../../Context/CalendarContext";
+import {format, parseISO} from "date-fns";
 
 const DateDetailsMedicationToTake = (props:ICalendarDay) => {
+
+    const {selectedDayDetails} = useContext(CalendarContext);
+
     return (
         <div>
             <Typography>Medications to take</Typography>
+            {
+                selectedDayDetails.map(medicationDosage=>
+
+                    {return !medicationDosage.hasBeenTaken ? <>{medicationDosage.prescriptionName}: {medicationDosage.amount} is scheduled to be taken at {format(parseISO(medicationDosage.time.toString()),'hh:mm aa').toString()}<br/></>:<></>}
+
+                )}
         </div>
     );
 };
--- backend/frontend/src/Components/CalendarPageComponets/DateDetails/DateDetailsMedicationsTaken.tsx
+++ backend/frontend/src/Components/CalendarPageComponets/DateDetails/DateDetailsMedicationsTaken.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {ICalendarDay} from "../../../../../Types/CalendarType";
 import {Typography} from "@mui/material";
+import {CalendarContext} from "../../../Context/CalendarContext";
 
 const DateDetailsMedicationsTaken = (props:ICalendarDay) => {
+
+    const {selectedDayDetails} = useContext(CalendarContext);
+
     return (
         <div>
             <Typography>Medications Taken</Typography>
+            {
+                selectedDayDetails.map(medicationDosage=>
+
+                {return medicationDosage.hasBeenTaken ? <>medicationDosage.prescriptionName +":"+ medicationDosage.amount+" has been taken"</>:<></>}
+
+            )}
         </div>
     );
 };
