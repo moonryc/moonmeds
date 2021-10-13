277dfaf39ae29c3226a7b235808dcb6ceaa4953a
Ryan Moon <rycmoon@gmail.com>
2021-10-13 11:39:44 -0500

    updated backend so that missed medications are marked missed

--- backend/Types/AlertMessageTypes.ts
+++ backend/Types/AlertMessageTypes.ts
@@ -2,11 +2,13 @@
  * This is an object that represents an alert
  * @property message - type:string
  * @property severity - type:string:"error" | "warning"| "info" | "success"
+ * @property numberOfOccurrences - type:number
  * @property notificationDate - type:Date
  */
 export interface IAlerts {
     message: string,
     severity: "error" | "warning" | "info" | "success",
+    numberOfOccurrences:number
     notificationDate: Date
 }
 
