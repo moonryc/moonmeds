503c1064b201a19a66b33a527cc12be0406e3d33
Ryan Moon <rycmoon@gmail.com>
2021-10-13 12:17:45 -0500

    removed unneeded import

--- backend/frontend/src/Pages/CalenderOverviewPage.tsx
+++ backend/frontend/src/Pages/CalenderOverviewPage.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import AppbarTop from "../Components/Standalone/AppbarTop";
-import {CalendarContainer} from "../Context/CalendarContext";
 import DisplayCalendarOverview from "../Components/CalendarPageComponets/DisplayCalendarOverview";
 import {makeStyles} from "@mui/styles";
 import NotificationsParent from "../Components/Misc/Notifications/NotificationsParent";
