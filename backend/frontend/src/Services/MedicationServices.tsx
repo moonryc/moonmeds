6bdeab563294cbcc85e51c966b481dbadaf1e18e
Ryan Moon <rycmoon@gmail.com>
2021-10-13 13:12:22 -0500

    changed value name to make use more clear

--- backend/frontend/src/Services/MedicationServices.tsx
+++ backend/frontend/src/Services/MedicationServices.tsx
@@ -3,16 +3,16 @@ import {IMedicationDosagesSchema} from "../../../Types/MedicationType";
 
 /**
  * Returns medication dosages pertaining to the selected date within the userMedicationDosages array
- * @param selectedDate - desired date
+ * @param selectedISODate - desired date
  * @param userMedicationDosages - the users medication dosages
  * @return []|IMedicationDosagesSchema[]
  */
-export const dosagesOnSpecifiedDay = (selectedDate:Date,userMedicationDosages:[]|IMedicationDosagesSchema[]):[]|IMedicationDosagesSchema[] => {
+export const dosagesOnSpecifiedDay = (selectedISODate:Date,userMedicationDosages:[]|IMedicationDosagesSchema[]):[]|IMedicationDosagesSchema[] => {
     if (userMedicationDosages == null) {
         return []
     }
 
-    let date = new Date(getYear(selectedDate), getMonth(selectedDate), getDate(selectedDate))
+    let date = new Date(getYear(selectedISODate), getMonth(selectedISODate), getDate(selectedISODate))
     let results = userMedicationDosages.filter(dosage => isEqual(date, new Date(
         getYear(parseISO(dosage.time.toString())),
         getMonth(parseISO(dosage.time.toString())),
