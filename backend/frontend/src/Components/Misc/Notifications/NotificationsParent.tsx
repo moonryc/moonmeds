f949eb9dba0db116adf8a79e930c98ce24449472
Ryan Moon <rycmoon@gmail.com>
2021-10-13 12:22:19 -0500

    reconfigured to use snackbar

--- backend/frontend/src/Components/Misc/Notifications/NotificationsParent.tsx
+++ backend/frontend/src/Components/Misc/Notifications/NotificationsParent.tsx
@@ -1,11 +1,13 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Snackbar, Stack} from "@mui/material";
-import {ApiContext} from "../../../Context/ApiContext";
-import {IAlerts} from "../../../../../Types/AlertMessageTypes";
-import Alert from "@mui/material/Alert";
-import CustomAlert from "./CustomAlert";
-import {AlertTitle} from "@mui/lab";
+import {Snackbar, SnackbarOrigin} from "@mui/material";
 import {NotificationsContext} from "../../../Context/NotificationsContext";
+import {IAlerts} from "../../../../../Types/AlertMessageTypes";
+import {Alert, AlertTitle} from "@mui/lab";
+
+
+export interface State extends SnackbarOrigin {
+    open: boolean;
+}
 
 /**
  * NotificationParent displays all notifications that the user has received
@@ -17,16 +19,55 @@ const NotificationsParent = () => {
     /**
      * gets the list of notifications from the notification context
      */
-    const {notifications} = useContext(NotificationsContext);
+    const {notifications, removeNotification} = useContext(NotificationsContext);
     //endregion
+
+    const [priorityNotification, setPriorityNotification] = useState<IAlerts | null>(null);
+
+    const handleClose = (event: React.SyntheticEvent | React.MouseEvent, reason?: string,) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        if (priorityNotification != null) {
+            removeNotification(priorityNotification)
+        }
+    };
+
+
+    useEffect(() => {
+        let errorAlerts = notifications.filter(alert => alert.severity === "error")
+        let warningAlerts = notifications.filter(alert => alert.severity === "warning");
+        let infoAlerts = notifications.filter(alert => alert.severity === "info");
+        let successAlerts = notifications.filter(alert => alert.severity === "success");
+
+        if (errorAlerts.length > 0) {
+            setPriorityNotification(errorAlerts[0])
+        } else if (warningAlerts.length > 0 && errorAlerts.length == 0) {
+            setPriorityNotification(warningAlerts[0])
+        } else if (infoAlerts.length > 0 && errorAlerts.length == 0 && warningAlerts.length == 0) {
+            setPriorityNotification(infoAlerts[0])
+        } else if (successAlerts.length > 0 && errorAlerts.length == 0 && warningAlerts.length == 0 && infoAlerts.length == 0) {
+            setPriorityNotification(successAlerts[0])
+        } else {
+            setPriorityNotification(null)
+        }
+        console.log(notifications)
+    }, [notifications]);
+
 
     return (
         <div>
-            <Stack sx={{width: "100%"}} spacing={2}>
-                {notifications.map((alert:IAlerts)=>{
-                    return <CustomAlert alert={alert}/>
-                })}
-            </Stack>
+            {priorityNotification != null ?
+                <Snackbar anchorOrigin={{vertical: "bottom", horizontal: "right"}} open={true} autoHideDuration={null}
+                          onClose={handleClose}>
+                    <Alert onClose={handleClose} severity={priorityNotification.severity} sx={{width: "100%"}}>
+                        {priorityNotification.severity == "error" ? <AlertTitle>Error</AlertTitle> : <></>}
+                        {priorityNotification.severity == "warning" ? <AlertTitle>Warning</AlertTitle> : <></>}
+                        {priorityNotification.message}
+                    </Alert>
+                </Snackbar> : <></>
+            }
         </div>
     );
 };
