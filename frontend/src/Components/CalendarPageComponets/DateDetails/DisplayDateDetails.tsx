14136df66983baec5d448351d7b31b676f52c6d3
moonryc <70920813+moonryc@users.noreply.github.com>
2021-11-29 14:05:53 -0600

    new branch

--- frontend/src/Components/CalendarPageComponets/DateDetails/DisplayDateDetails.tsx
+++ frontend/src/Components/CalendarPageComponets/DateDetails/DisplayDateDetails.tsx
@@ -6,7 +6,7 @@ import {
   AccordionSummary,
   Box,
   Button,
-  Chip,
+  Chip, Divider, List, ListItem, ListItemButton, ListItemText, ListSubheader,
   Typography,
 } from "@mui/material";
 import { differenceInDays, format, parseISO, toDate } from "date-fns";
@@ -64,359 +64,65 @@ const DisplayDateDetails:React.FC<IDisplayDateDetailsProp> = ({selectedDate}) =>
 return (
     <Box ref={ref}
       
-      sx={{padding: "3vh", height:'100%' }}
+      sx={{padding: "3vh", height:'100%', position:"relative"}}
     >
       <Typography variant={"h4"} sx={{ ...titleStyle, ...centeredTextStyle }}>
         {" "}
         Date Details
       </Typography>
       <Typography variant={"h5"}>{date.toString()}</Typography>
+      <a href={"#section2"}> <button>test sticky 4</button></a>
 
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Medications Taken</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography
-            variant={"body2"}
-            sx={{ fontSize: "25px", color: "text.primary" }}
-          >
-            {selectedDayDetails.map((medicationDosage,index) => {
-              return medicationDosage.hasBeenTaken ? (
-                <div key={index}>
-                  <Box
-                    sx={{
-                      bgcolor: "green",
-                      width: "100%",
-                      height: '100%',
-                      borderRadius: 2,
-                      position: "relative",
-                      display:'flex',
-                      alignContent:'center'
-                    }}
-                  >
-                    <Typography
-                      sx={{
-                        marginLeft: "1vh",
-                        fontSize: "15px",
-                        top: "22.5%",
-                        position: "relative",
-                        width:'70%',
-                        flex:'1',
-                        p:'12px',
-                        lineHeight:'35px'
+      <List
+          sx={{
+            width: '100%',
+            height: '60vh',
+            bgcolor: 'background.paper',
+            position: 'relative',
+            overflow: 'auto',
 
-                      }}
-                    >
-                      <Chip
-                        sx={{ bgcolor: medicationDosage.medicationOwner.color }}
-                        icon={<Face />}
-                        //@ts-ignore
-                        label={truncateString(medicationDosage.medicationOwner.name)}
-                        title={medicationDosage.medicationOwner.name}
-                      />
-                      {"  "}
-                      <Chip
-                        sx={{ bgcolor: medicationDosage.medicationOwner.color }}
-                        icon={<MedicationIcon />}
-                        //@ts-ignore
-                        //label={medicationDosage.prescriptionName.slice(0,document.getElementById("box").parentElement.clientWidth/50)}
-                         label={truncateString(medicationDosage.prescriptionName)}
-                        title={medicationDosage.prescriptionName}
-                      />
-                      {"  Dosage was taken at "}
-                      {format(parseISO(medicationDosage.timeTaken), "h:mm aa")}
-                    </Typography>
-                    <Button
-                      variant={"contained"}
-                      sx={{m:'1vw', }}
-                      onClick={() =>
-                        putUpdateMedicationDosage(
-                          medicationDosage.dosageId,
-                          false,
-                          medicationDosage.hasBeenMissed,
-                          new Date()
-                        )
-                      }
-                    >
-                      Undo
-                    </Button>
-                  </Box>
-
-                  <br />
-                </div>
-              ) : (
-                <span  />
-              );
-            })}
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Medications To Take</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography
-            variant={"body2"}
-            sx={{ fontSize: "25px", color: "text.primary" }}
-          >
-            {selectedDayDetails.map((medicationDosage,index) => {
-              return !medicationDosage.hasBeenTaken &&
-                !medicationDosage.hasBeenMissed ? (
-                <div key={index}>
-                  <Box
-                    sx={{
-                      bgcolor: "orange",
-                      width: "100%",
-                      height: '100%',
-                      borderRadius: 2,
-                      position: "relative",
-                      display:'flex',
-                      alignContent:'center'
-                    }}
-                  >
-                    <Typography
-                      sx={{
-                        marginLeft: "1vh",
-                        fontSize: "15px",
-                        top: "22.5%",
-                        position: "relative",
-                        width:'70%',
-                        flex:'1',
-                        p:'12px',
-                        lineHeight:'35px'
-                      }}
-                    >
-                      <Chip
-                        sx={{ bgcolor: medicationDosage.medicationOwner.color }}
-                        icon={<Face />}
-                        //@ts-ignore
-                        label={truncateString(medicationDosage.medicationOwner.name)}
-                        title={medicationDosage.medicationOwner.name}
-                      />
-                      {"  "}
-                      <Chip
-                        sx={{ bgcolor: medicationDosage.medicationOwner.color }}
-                        icon={<MedicationIcon />}
-                        //@ts-ignore
-                        label={truncateString(medicationDosage.prescriptionName)}
-                        title={medicationDosage.prescriptionName}
-                      />
-                      {" Dosage to be taken at "}
-                      {format(
-                        parseISO(medicationDosage.timeToTake.toString()),
-                        "h:mm aa"
-                      ).toString()}
-                    </Typography>
-                    <Button
-                      variant={"contained"}
-                      sx={{m:'1vw', }}
-                      onClick={() =>
-                        putUpdateMedicationDosage(
-                          medicationDosage.dosageId,
-                          true,
-                          medicationDosage.hasBeenMissed,
-                          new Date()
-                        )
-                      }
-                    >
-                      Mark as Taken
-                    </Button>
-                  </Box>
-                  <br />
-                </div>
-              ) : (
-                <span  />
-              );
-            })}
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Missed Medications</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography
-            variant={"body2"}
-            sx={{ fontSize: "25px", color: "text.primary" }}
-          >
-            {selectedDayDetails.map((medicationDosage,index) => {
-              return medicationDosage.hasBeenMissed &&
-                !medicationDosage.hasBeenTaken ? (
-                <div key={index}>
-                  <Box
-
-                    sx={{
-                      bgcolor: "red",
-                      width: "100%",
-                      height: '100%',
-                      borderRadius: 2,
-                      position: "relative",
-                      display:'flex',
-                      alignContent:'center'
-                    }}
-                  >
-                    <Typography
-                      sx={{
-                        marginLeft: "1vh",
-                        fontSize: "15px",
-                        top: "22.5%",
-                        position: "relative",
-                        width:'70%',
-                        flex:'1',
-                        p:'12px',
-                        lineHeight:'35px'
-
-
-                      }}
-                    >
-                      <Chip
-                        sx={{ bgcolor: medicationDosage.medicationOwner.color }}
-                        icon={<Face />}
-                        //@ts-ignore
-                        label={truncateString(medicationDosage.medicationOwner.name)}
-                        title={medicationDosage.medicationOwner.name}
-                      />
-                      {"  "}
-                      <Chip
-                        sx={{ bgcolor: medicationDosage.medicationOwner.color }}
-                        icon={<MedicationIcon />}
-                        //@ts-ignore
-                        label={truncateString(medicationDosage.prescriptionName)}
-                        title={medicationDosage.prescriptionName}
-                      />
-                      {" Dosage was missed at "}
-                      {format(
-                        parseISO(medicationDosage.timeToTake.toString()),
-                        "h:mm aa"
-                      ).toString()}
-                    </Typography>
-                    <Button
-                      variant={"contained"}
-                       sx={{m:'1vw', }}
-                      onClick={() =>
-                        putUpdateMedicationDosage(
-                          medicationDosage.dosageId,
-                          true,
-                          medicationDosage.hasBeenMissed,
-                          new Date()
-                        )
-                      }
-                    >
-                      Mark as taken
-                    </Button>
-                  </Box>
-                  <br />
-                </div>
-              ) : (
-                <span  />
-              );
-            })}
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Medications To Refill</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography
-            variant={"body2"}
-            sx={{ fontSize: "25px", color: "text.primary" }}
-          >
-            {selectedDayDetails.map((medicationDosage,index) => {
-              const numberOfDaysBeforeRefill = differenceInDays(
-                new Date(medicationDosage.nextFillDay),
-                new Date(medicationDosage.timeToTake)
-              );
-              return (
-                <div key={index}>
-                  {numberOfDaysBeforeRefill <= 7 ? (
+            '& ul': { padding: 0 },
+          }}
+          subheader={<li />}
+      >
+        {[0, 1, 2, 3, 4].map((sectionId) => (
+            <li key={`section-${sectionId}`}>
+              <ul>
+                <ListSubheader id={"section"+sectionId}>{`I'm sticky ${sectionId}`}</ListSubheader>
+                <Divider/>
+                {[0, 1, 2,3].map((item) => (
                     <>
-                      <Box
-                        
-                        sx={{
-                          bgcolor: "blue",
-                          width: "100%",
-                          height: '100%',
-                          borderRadius: 2,
-                          position: "relative",
-                          display:'flex',
-                          alignContent:'center'
-                        }}
-                      >
-                        <Typography
-                          sx={{
-                            marginLeft: "1vh",
-                            fontSize: "15px",
-                            top: "22.5%",
-                            position: "relative",
-                            width:'70%',
-                            flex:'1',
-                            p:'12px',
-                            lineHeight:'35px'
-                          }}
-                        >
-                          <Chip
-                            sx={{
-                              bgcolor: medicationDosage.medicationOwner.color,
-                            }}
-                            icon={<Face />}
-                            label={truncateString(medicationDosage.medicationOwner.name)}
-                            title={medicationDosage.medicationOwner.name}
-                          />
-                          {"  "}
-                          <Chip
-                            sx={{
-                              bgcolor: medicationDosage.medicationOwner.color,
-                            }}
-                            icon={<MedicationIcon />}
-                            label={truncateString(medicationDosage.prescriptionName)}
-                            title={medicationDosage.prescriptionName}
-                          />
-                          {" refill in " +
-                            differenceInDays(
-                              new Date(medicationDosage.nextFillDay),
-                              new Date()
-                            ) +
-                            " days"}
-                        </Typography>
-                        <Button
-                          variant={"contained"}
-                          sx={{m:'1vw', }}
-                        >
-                          Refill
-                        </Button>
-                      </Box>
-                      <br />
+                      <ListItemButton>
+                    <ListItem key={`item-${sectionId}-${item}`}>
+                      <ListItemText primary={`Item ${item}`} />
+                    </ListItem>
+                    </ListItemButton>
+                    <Divider />
                     </>
-                  ) : (
-                    <></>
-                  )}
-                </div>
-              );
-            })}
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+                ))}
+                <ListItemButton>
+                  <ListItem key={`item-${sectionId}-${156}`}>
+                    <ListItemText primary={`Item ${456}`} />
+                  </ListItem>
+                </ListItemButton>
+                <Divider />
+                <ListItemButton>
+                  <ListItem key={`item-${sectionId}-${5615665}`}>
+                    <ListItemText primary={`Item ${51561}`} />
+                  </ListItem>
+                </ListItemButton>
+                <Divider />
+                <ListItemButton>
+                  <ListItem key={`item-${sectionId}-${2664865465}`}>
+                    <ListItemText primary={`Item ${1566156165}`} />
+                  </ListItem>
+                </ListItemButton>
+                <Divider />
+              </ul>
+            </li>
+        ))}
+      </List>
+
     </Box>
   );
 };
