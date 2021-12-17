cc474e239c7e93c4b44958775b48924ccd70a94f
moonryc <70920813+moonryc@users.noreply.github.com>
2021-12-16 18:16:29 -0600

    removed unwanted comments

--- frontend/src/Components/CalendarPageComponets/Calendar/CalendarDay.tsx
+++ frontend/src/Components/CalendarPageComponets/Calendar/CalendarDay.tsx
@@ -19,120 +19,15 @@ const CalendarDay = (props: ICalendarDay & { isRenderedOnHomePage: boolean }) =>
         //endregion
 
         /**
-         * the medication dosages associated with this day in particular
-         * TODO: This is to be used for styling
-         *
-         */
-        const [medicationDosagesDetails, setMedicationDosagesDetails] = useState<IMedicationDosagesBase[] | []>(dosagesOnSpecifiedDay(props.date, userMedicationDosages));
-        // const [calendarDayColor, setCalendarDayColor] = useState({
-        //     bgcolor: "primary.main",
-        //     width: ['40px'],
-        //     height: ['40px'],
-        //     color: "rgba(0,0,0,0.83)",
-        //     border: 0,
-        //     borderRadius: "50%",
-        //     borderColor: "primary.main",
-        // });
-        /**
          * updates calendar context regarding what day is currently selected
          * and sets the context details to whatever the
          * selected days medicationDetails are
          */
         const handleOnDayClick = () => {
             setSelectedDay(props.date);
-            console.log(medicationDosagesDetails);
+
             // console.log(filteredFutureDosages)
         };
-
-        // const isToday = useCallback((): boolean => {
-        //   return isSameDay(new Date(props.date), new Date());
-        // }, [props.date]);
-        //
-        // const isFillDay = useCallback((): any => {
-        //   if (medicationDosagesDetails != null) {
-        //     for (let i in medicationDosagesDetails) {
-        //       //@ts-ignore
-        //       if (
-        //         isSameDay(
-        //           new Date(medicationDosagesDetails[i].nextFillDay),
-        //           new Date(props.date)
-        //         )
-        //       ) {
-        //         return true;
-        //       }
-        //       //ts-ignore
-        //       console.log(medicationDosagesDetails[0].nextFillDay);
-        //     }
-        //   }
-        //   // console.log(new Date(props.date))
-        //   return false;
-        // }, [medicationDosagesDetails, props.date]);
-        //
-        // const isMissedDate = useCallback((): any => {
-        //   if (medicationDosagesDetails != null) {
-        //     for (let i in medicationDosagesDetails) {
-        //       if (medicationDosagesDetails[i].hasBeenMissed) {
-        //         return true;
-        //       }
-        //     }
-        //   }
-        //   return false;
-        // }, [medicationDosagesDetails]);
-        //
-        // const cssClassUpdater = useCallback(() => {
-        //   let isItToday = isToday();
-        //   let isDayAFillday = isFillDay();
-        //   let isMissed = isMissedDate();
-        //
-        //   if (isItToday) {
-        //     setCalendarDayColor({
-        //       bgcolor: "primary.main",
-        //       border: 1,
-        //       width: ['40px'],
-        //       height: ['40px'],
-        //       borderRadius: "50%",
-        //       color: "text.primary",
-        //       borderColor: "primary.light",
-        //     });
-        //     return;
-        //   }
-        //
-        //   if (isMissed) {
-        //     setCalendarDayColor({
-        //       bgcolor: "primary.main",
-        //       width: ['40px'],
-        //       height: ['40px'],
-        //       color: "missed.main",
-        //       border: 0,
-        //       borderRadius: "50%",
-        //       borderColor: "primary.main",
-        //     });
-        //     return;
-        //   }
-        //
-        //   if (isDayAFillday) {
-        //     setCalendarDayColor({
-        //       bgcolor: "primary.main",
-        //       width: ['40px'],
-        //       height: ['40px'],
-        //       color: "refills.main",
-        //       border: 0,
-        //       borderRadius: "50%",
-        //       borderColor: "primary.main",
-        //     });
-        //     return;
-        //   }
-        //
-        //   // setCalendarDayColor({
-        //   //   bgcolor: "primary.main",
-        //   //   width: ['40px'],
-        //   //   height: ['40px'],
-        //   //   color: "text.primary",
-        //   //   border: 0,
-        //   //   borderRadius: "50%",
-        //   //   borderColor: "primary.main",
-        //   // });
-        // }, [isFillDay, isMissedDate, isToday]);
 
 
         //#region class testers for calendar day color
@@ -179,8 +74,9 @@ const CalendarDay = (props: ICalendarDay & { isRenderedOnHomePage: boolean }) =>
                 return false;
             }
         }
-//#endregion
-//#region class updaters for styles
+        //#endregion
+
+        //#region class updaters for styles
         const selectedDayColor = () => {
             if (checkIsSelectedDay() && checkIsToday()) {
                 return
@@ -201,17 +97,18 @@ const CalendarDay = (props: ICalendarDay & { isRenderedOnHomePage: boolean }) =>
                     transform: 'translate(-2px,0px)'
                 })
             }
-            if(checkIsToday()){
+            if (checkIsToday()) {
                 return ({
                     border: 1,
                     borderColor: 'text.secondary',
                     borderRadius: '5px',
                     transform: 'translate(0px,2px)'
                 })
-            }return({
+            }
+            return ({
                 border: 3,
-                borderColor:'primary.main',
-                borderRadius:'5px',
+                borderColor: 'primary.main',
+                borderRadius: '5px',
             })
         }
         const alertStyling = () => {
@@ -226,27 +123,27 @@ const CalendarDay = (props: ICalendarDay & { isRenderedOnHomePage: boolean }) =>
                 })
             }
         }
-//#endregion
+        //#endregion
 
         return (
-            <Box sx={{display:'flex', ...centeredTextStyle, width: '40px', height: '40px'}}>
-                <Box sx={{...isTodayStyling(),...centeredTextStyle, height:'37px',width:'37px'}}>
-                <IconButton
-                    sx={{
-                        padding: 0,
-                        height: '37px',
-                        width: '37px',
-                        color: 'text.primary',
-                        ...selectedDayColor(),
-                        ...alertStyling(),
-                        fontSize:'20px'
-                    }}
-                    onClick={() => {
-                        handleOnDayClick();
-                    }}
-                >
-                    {getDate(props.date)}
-                </IconButton>
+            <Box sx={{display: 'flex', ...centeredTextStyle, width: '40px', height: '40px'}}>
+                <Box sx={{...isTodayStyling(), ...centeredTextStyle, height: '37px', width: '37px'}}>
+                    <IconButton
+                        sx={{
+                            padding: 0,
+                            height: '37px',
+                            width: '37px',
+                            color: 'text.primary',
+                            ...selectedDayColor(),
+                            ...alertStyling(),
+                            fontSize: '20px'
+                        }}
+                        onClick={() => {
+                            handleOnDayClick();
+                        }}
+                    >
+                        {getDate(props.date)}
+                    </IconButton>
                 </Box>
             </Box>
         );
