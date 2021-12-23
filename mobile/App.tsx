58312764b22a65631b0cf7a5ea33d98c1f725583
moonryc <70920813+moonryc@users.noreply.github.com>
2021-12-23 15:16:13 -0600

    Added native base provider and native safe area provider

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,25 +6,32 @@ import StoreContextContainer from "./Store/StoreContext";
 import {ApplicationProvider, Layout} from "@ui-kitten/components";
 import * as eva from '@eva-design/eva';
 import Navigation from "./Navigation/Navigation";
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from "@react-navigation/native";
+import {NativeBaseProvider} from 'native-base';
+
 
 export default function App() {
     return (
         <ApplicationProvider {...eva} theme={eva.dark}>
             {/*{eva.dark} for dark mode*/}
-        <SafeAreaProvider>
-            <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <UserContextContainer>
-                    <MedicationContextContainer>
-                        <ApiContextContainer>
-                            <StoreContextContainer>
-                                <Navigation/>
-                            </StoreContextContainer>
-                        </ApiContextContainer>
-                    </MedicationContextContainer>
-                </UserContextContainer>
-            </Layout>
-        </SafeAreaProvider>
+            <NativeBaseProvider>
+                <SafeAreaProvider>
+                    <NavigationContainer>
+                        <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                            <UserContextContainer>
+                                <MedicationContextContainer>
+                                    <ApiContextContainer>
+                                        <StoreContextContainer>
+                                            <Navigation/>
+                                        </StoreContextContainer>
+                                    </ApiContextContainer>
+                                </MedicationContextContainer>
+                            </UserContextContainer>
+                        </Layout>
+                    </NavigationContainer>
+                </SafeAreaProvider>
+            </NativeBaseProvider>
         </ApplicationProvider>
     );
 }
